f26c78c75f343d6ec4625953437b0ceb457d31c0
Monika Marciniak <monixxa90@gmail.com>
2022-09-04 21:07:12 +0200

    zmiany

--- src/index.js
+++ src/index.js
@@ -1,115 +1,143 @@
-import './sass/index.scss';
-import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { getImages } from './GetImages';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
 
-const searchForm = document.querySelector('#search-form');
+const form = document.querySelector('.search-form');
+const input = document.querySelector('input');
+const searchBtn = document.querySelector(`button[type="submit"]`);
 const gallery = document.querySelector('.gallery');
-const loadMoreButton = document.querySelector('.load-more');
 
-//eventListeners
-
-searchForm.addEventListener('submit', searchImage);
-loadMoreButton.addEventListener('click', moreImages);
-
-// variables 
-
-let query = '';
-let page = 1;
+const API_KEY = '29711203-05eaaaab908cc3e1419fc45fb';
 const perPage = 40;
-let lightbox = new SimpleLightbox('.gallery a');
+let page = 1;
+const lightbox = new simpleLightbox('.gallery a');
 
-//functions
+const fetchPhotos = async (search, pagenr) => {
+  const response = await fetch(
+    `https://pixabay.com/api/?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&pretty=true&per_page=${perPage}&page=${pagenr}`
+  );
+  const photos = await response.json();
+  return photos;
+};
 
-function clearResults() {
-  gallery.innerHTML = '';
-}
-
-function searchImage(event) {
-  event.preventDefault();
-  query = event.currentTarget.searchQuery.value.trim();
-  loadMoreButton.classList.add('is-hidden');
-  clearResults();
-  if (!query) {
-    Notiflix.Notify.info(
-          "Hello Dear, You came here to find some image, don't you?:)."
-        );
-    return;
-  }
-
-  getImages(query, page, perPage)
-    .then(({ data }) => {
-      if (!data.totalHits) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      else {
-        renderGallery(data.hits);
-        lightbox.refresh();
-      
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        if (data.totalHits > perPage) {
-          loadMoreButton.classList.remove('is-hidden');
-        }
-      }
-    })
-    .catch(error => console.log(error));
-}
-
-function moreImages() {
-  page += 1;
-getImages(query, page, perPage)
-  .then(({ data }) => {
-    renderGallery(data.hits);
-      lightbox.refresh();
-    const allPages = Math.ceil(data.hits / perPage);
-    
-      if (page > allPages) {
-        loadMoreButton.classList.add('is-hidden');
-        Notiflix.Notify.failure(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    })
-    .catch(error => console.log(error));
-}
-
-//rendering
-function renderGallery(data) {
-  const markup = data
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<div class="photo-card">
-        <a href="${largeImageURL}"> <img src="${webformatURL}" alt="${tags}" loading="lazy" title=""/></a>
+const renderGallery = dataArray => {
+  let markup = dataArray
+    .map(data => {
+      return `<div class="photo-card">
+  <a href="${data.largeImageURL}"><img src="${data.webformatURL}" alt="${data.tags}" loading="lazy" title="" /></a>
   <div class="info">
     <p class="info-item">
-      <b>Likes</b>${likes}</p>
+      <b>Likes</b>
+      <span class="span-item">${data.likes}</span>
+    </p>
     <p class="info-item">
-      <b>Views</b>${views}</p>
+      <b>Views</b>
+      <span class="span-item">${data.views}</span>
+    </p>
     <p class="info-item">
-      <b>Comments</b>${comments}</p>
+      <b>Comments</b>
+      <span class="span-item">${data.comments}</span>
+    </p>
     <p class="info-item">
-      <b>Downloads</b>${downloads}</p>
+      <b>Downloads</b>
+      <span class="span-item">${data.downloads}</span>
+    </p>
   </div>
 </div>`;
-      }
-    )
+    })
     .join('');
 
-  gallery.insertAdjacentHTML('beforeend', markup);
-}
+  gallery.innerHTML = markup;
+};
 
-//On scroll to TOP
-let topBtn = document.querySelector(".top-btn");
-topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
-window.onscroll = () => window.scrollY > 500 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0
+const renderNextPhotos = dataArray => {
+  let markup2 = dataArray
+    .map(data => {
+      return `<div class="photo-card">
+  <a href="${data.largeImageURL}"><img src="${data.webformatURL}" alt="${data.tags}" loading="lazy" title="" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <span class="span-item">${data.likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <span class="span-item">${data.views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <span class="span-item">${data.comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <span class="span-item">${data.downloads}</span>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup2);
+};
+
+form.addEventListener('submit', async event => {
+  try {
+    event.preventDefault();
+    const { searchBtn, searchQuery } = event.currentTarget;
+    page = 1;
+    let trimInput = searchQuery.value.trim();
+    if (trimInput === '') {
+      return;
+    }
+    localStorage.setItem('inputValue', `${trimInput}`);
+    gallery.innerHTML = '';
+    const varPhotos = await fetchPhotos(trimInput, page);
+    const photosArr = varPhotos.hits;
+    const total = varPhotos.totalHits;
+    if (total > 0) {
+      Notiflix.Notify.success(`Hooray! We found ${total} images.`);
+    }
+    if (photosArr.length === 0) {
+      throw new Error();
+    }
+    renderGallery(photosArr);
+    lightbox.refresh();
+  } catch (error) {
+    gallery.innerHTML = '';
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+});
+
+window.addEventListener(
+  'scroll',
+  debounce(async event => {
+    try {
+      if (window.innerHeight === document.documentElement.scrollHeight) {
+        return;
+      }
+      if (
+        window.scrollY + 0.5 + window.innerHeight >=
+        document.documentElement.scrollHeight
+      ) {
+        page += 1;
+        let trimInput = localStorage.getItem('inputValue');
+        const varPhotos = await fetchPhotos(trimInput, page);
+        const photosArr = varPhotos.hits;
+
+        renderNextPhotos(photosArr);
+        lightbox.refresh();
+        const { height: cardHeight } = document
+          .querySelector('.gallery')
+          .firstElementChild.getBoundingClientRect();
+        window.scrollBy({
+          top: cardHeight * 1.5,
+          behavior: 'smooth',
+        });
+      }
+    } catch (error) {}
+  }, 100)
+);
